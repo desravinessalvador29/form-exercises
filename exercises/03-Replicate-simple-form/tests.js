59bc8bf993e5ee12de003725310f049e6f1ccb27
Salvador Desravines <desravines.salvador001@yahoo.com>
2019-11-27 00:13:59 +0000

    finished 03

--- exercises/03-Replicate-simple-form/tests.js
+++ exercises/03-Replicate-simple-form/tests.js
@@ -36,15 +36,70 @@ describe('basic HTML structure', function () {
      let a = document.documentElement.innerHTML = html.toString()
            expect(a.indexOf("<head>")).not.toBe(-1)
     })
-    it('<title> tag exists and the innerHTML needs to be "Hello World"', function () {
-     let a = document.documentElement.innerHTML = html.toString()
-           expect(a.indexOf("<title>")).not.toBe(-1)
-           expect(document.querySelector("title").innerHTML).toBe("Hello World")
-    })
+    // it('<title> tag exists and the innerHTML needs to be "Hello World"', function () {
+    //  let a = document.documentElement.innerHTML = html.toString()
+    //        expect(a.indexOf("<title>")).not.toBe(-1)
+    //        expect(document.querySelector("title").innerHTML).toBe("Hello World")
+    // })
       it('<body> tag exists', function () {
      let a = document.documentElement.innerHTML = html.toString()
            expect(a.indexOf("<body")).not.toBe(-1)
     })
 
 
+
+    it('you should create a <form> tag', function () {
+    let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<form")).not.toBe(-1)
+    })
+
+    it('you should create a <tr> tag', function () {
+    let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<tr")).not.toBe(-1)
+    })
+
+    it('you should have 2 <th> tags', function () {
+    let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<th")).not.toBe(-1)
+    })
+
+    it('you should have an <input> tags', function () {
+        let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<input")).not.toBe(-1)
+    })
+
+    it('You should create an input type=radio with value male', function () {
+        let inputs = document.querySelectorAll("form input");
+        let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<input")).not.toBe(-1)
+        expect(inputs[1].type).toBe("radio");
+        expect(inputs[1].value).toBe("male");
+    })
+
+    it('You should create an input type=radio with value female', function () {
+        let inputs = document.querySelectorAll("form input");
+        let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<input")).not.toBe(-1)
+        expect(inputs[2].type).toBe("radio");
+        expect(inputs[2].value).toBe("female");
+    })
+
+    it('you should have an <select> tags', function () {
+        let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<select")).not.toBe(-1)
+    })
+
+    it('you should have a second <input> tag', function () {
+        let inputs = document.querySelectorAll("form input");
+        let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<input")).not.toBe(-1)
+        expect(inputs[3].type).toBe("checkbox");
+    })
+
+    it('you should have an <textarea> tags', function () {
+        let a = document.documentElement.innerHTML = html.toString()
+        expect(a.indexOf("<textarea")).not.toBe(-1)
+    }) 
+
 });
+
